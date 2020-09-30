a2400b00b62f393a10a34e2384820b082a41cfc6
nick-brook <66472330+nick-brook@users.noreply.github.com>
2020-09-30 18:51:07 +0100

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,28 @@
+
+
+const dataArr = [
+                {   id : "Q",
+                    text: "test",
+                    audio: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"},
+
+                {   id : "W",
+                    text: "W",
+                    audio: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"}]
+
+
+function padSound(id) {
+    var disptext = "";
+    var audioObj = new Audio(dataArr[0].audio);
+
+    dispText = dataArr.filter(item =>)
+
+
+    document.getElementById("display").innerHTML = dispText;
+
+audioObj.play()
+
+
+}
+
+}
+
